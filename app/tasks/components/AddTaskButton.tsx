374a7117bd5294d233ed61388bc2472b92addbe5
Vladimir Hejrosky <vladimir.hejrosky@gmail.com>
2025-03-12 14:44:32 +0100

    Reformated AddTaskButton

--- app/tasks/components/AddTaskButton.tsx
+++ app/tasks/components/AddTaskButton.tsx
@@ -1,35 +1,31 @@
 "use client"
-import { IconButton, Paper } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
-import React from "react";
+import { Fab } from "@mui/material";
 
 const AddTaskButton = () => {
   return (
-    <Paper elevation={3} variant="outlined" sx={{
-        position: "fixed",
-        left: 5,
-        bottom: 65,
-        opacity: ".8",
-        borderRadius: "100%",
-        display: {xs: "block", lg: "none"},
-        boxShadow: "0 0 5px var(--mui-palette-common-onBackground)", 
-        "@keyframes glowing": {
-          "0%": {
-            boxShadow: "0 0 5px var(--mui-palette-common-onBackground)", 
-          },
-          "50%": {
-            boxShadow: "0 0 15px var(--mui-palette-common-onBackground)", 
-          },
-          "100%": {
-            boxShadow: "0 0 5px var(--mui-palette-common-onBackground)", 
-          },
+    <Fab color="secondary" sx={{
+      position: "fixed",
+      left: 5,
+      bottom: 65,
+      opacity: ".9",
+      display: {xs: "block", lg: "none"},
+      boxShadow: "0 0 5px var(--mui-palette-common-onBackground)", 
+      "@keyframes glowing": {
+        "0%": {
+          boxShadow: "0 0 5px var(--mui-palette-common-onBackground)", 
         },
-        animation: "glowing 1.5s infinite", 
-      }}>
-      <IconButton>
-        <AddIcon fontSize="large" />
-      </IconButton>
-    </Paper>
+        "50%": {
+          boxShadow: "0 0 10px var(--mui-palette-common-onBackground)", 
+        },
+        "100%": {
+          boxShadow: "0 0 5px var(--mui-palette-common-onBackground)", 
+        },
+      },
+      animation: "glowing 1.5s infinite", 
+    }}>
+      <AddIcon />
+    </Fab>
   );
 };
 
