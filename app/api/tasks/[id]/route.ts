62939577f75d41a09703968d8a806e04e758c3e3
Vladimir Hejrosky <vladimir.hejrosky@gmail.com>
2025-03-26 18:01:21 +0100

    Remove async get params in APIs

--- app/api/tasks/[id]/route.ts
+++ app/api/tasks/[id]/route.ts
@@ -1,21 +1,21 @@
 import { prisma } from "@/prisma/prisma";
-import { Repeat } from "@prisma/client"
+import { Repeat } from "@prisma/client";
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 import { authOptions } from "../../auth/[...nextauth]/route";
 
-interface patchData{
-  label?: string,
-  done?: boolean,
-  repeat?: Repeat,
-  priority?: number
-} 
+interface patchData {
+  label?: string;
+  done?: boolean;
+  repeat?: Repeat;
+  priority?: number;
+}
 
 export async function DELETE(
   req: NextRequest,
-  { params }: { params: Promise<{ id: string }> }
+  { params }: { params: { id: string } }
 ) {
-    const {id} = await params
+  const { id } = params;
   const session = await getServerSession(authOptions);
   if (!session || !session.user?.email) {
     return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
@@ -27,102 +27,127 @@ export async function DELETE(
   }
 
   try {
-  const user = await prisma.user.findUnique({
-    where: { email: session.user.email },
-    include: {
-      task: { where: {id: taskId} },
-    },
-  });
+    const user = await prisma.user.findUnique({
+      where: { email: session.user.email },
+      include: {
+        task: { where: { id: taskId } },
+      },
+    });
 
-  if (!user || user.task.length === 0) {
-    return NextResponse.json({ error: "Task not found or not owned by user" }, { status: 404 });
+    if (!user || user.task.length === 0) {
+      return NextResponse.json(
+        { error: "Task not found or not owned by user" },
+        { status: 404 }
+      );
+    }
+
+    await prisma.task.delete({
+      where: { id: taskId },
+    });
+
+    return NextResponse.json(
+      { message: "Task deleted successfully" },
+      { status: 200 }
+    );
+  } catch {
+    return NextResponse.json(
+      { error: "Failed to delete task" },
+      { status: 500 }
+    );
   }
-
-  await prisma.task.delete({
-    where: { id: taskId },
-  });
-
-  return NextResponse.json({ message: "Task deleted successfully" }, { status: 200 });
-
-} catch{
-  return NextResponse.json({ error: "Failed to delete task" }, { status: 500 });
-}
 }
 
-export async function PATCH(req: NextRequest, {params}: {params: Promise<{id: string}>}) {
-  const { id } = await params
-  const session = await getServerSession(authOptions)
-  const {label, repeat, priority, done}:patchData = await req.json()
+export async function PATCH(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const { id } = params;
+  const session = await getServerSession(authOptions);
+  const { label, repeat, priority, done }: patchData = await req.json();
 
   const updateData: patchData = {
     label,
     repeat,
     priority,
-    done
-  }
-  console.log(updateData)
+    done,
+  };
+  console.log(updateData);
 
   if (!session || !session.user?.email) {
-    return NextResponse.json({error: "Unauthorized"}, {status: 401})
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
   }
 
-  const taskId = parseInt(id, 10)
-  if (isNaN(taskId)){
-    return NextResponse.json({error: "Invalid Task ID"}, {status: 400})
+  const taskId = parseInt(id, 10);
+  if (isNaN(taskId)) {
+    return NextResponse.json({ error: "Invalid Task ID" }, { status: 400 });
   }
 
   try {
     const user = await prisma.user.findUnique({
-      where: {email: session.user.email},
+      where: { email: session.user.email },
       include: {
-        task: {where: { id: taskId}}
-      }
-    })
+        task: { where: { id: taskId } },
+      },
+    });
 
     if (!user || user.task.length === 0) {
-      return NextResponse.json({error: "Taks not found or not owned by user"}, {status: 404})
+      return NextResponse.json(
+        { error: "Taks not found or not owned by user" },
+        { status: 404 }
+      );
     }
-  
+
     await prisma.task.update({
       where: { id: taskId },
-      data: updateData
-    })
+      data: updateData,
+    });
 
-    return NextResponse.json({message: "Task updated succesfully"}, {status: 200})
+    return NextResponse.json(
+      { message: "Task updated succesfully" },
+      { status: 200 }
+    );
   } catch {
-    return NextResponse.json({error: "Failed to edit task,"}, {status: 500})
+    return NextResponse.json(
+      { error: "Failed to edit task," },
+      { status: 500 }
+    );
   }
 }
 
-export async function GET(req: NextRequest, {params}: {params: Promise<{id: string}>}) {
-  const { id } = await params
-  const session = await getServerSession(authOptions)
+export async function GET(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const { id } = params;
+  const session = await getServerSession(authOptions);
 
   if (!session || !session.user?.email) {
-    return NextResponse.json({error: "Unauthorized"}, {status: 401})
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
   }
 
-  const taskId = parseInt(id, 10)
-  if (isNaN(taskId)){
-    return NextResponse.json({error: "Invalid Task ID"}, {status: 400})
+  const taskId = parseInt(id, 10);
+  if (isNaN(taskId)) {
+    return NextResponse.json({ error: "Invalid Task ID" }, { status: 400 });
   }
 
   try {
     const user = await prisma.user.findUnique({
-      where: {email: session.user.email},
+      where: { email: session.user.email },
       include: {
-        task: { where: {id: taskId}}
-      }
-    })
+        task: { where: { id: taskId } },
+      },
+    });
 
     if (!user || user.task.length === 0) {
-      return NextResponse.json({error: "Taks not found or not owned by user"}, {status: 404})
+      return NextResponse.json(
+        { error: "Taks not found or not owned by user" },
+        { status: 404 }
+      );
     }
-    const task = user.task[0] || null
+    const task = user.task[0] || null;
 
-    return NextResponse.json(task, {status: 200})
-
+    return NextResponse.json(task, { status: 200 });
   } catch {
-    return NextResponse.json({error: "Failed to get task,"}, {status: 500})
+    return NextResponse.json({ error: "Failed to get task," }, { status: 500 });
   }
 }
