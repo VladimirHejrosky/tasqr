de0b3efaa66bd808533ec4caa2f3571157d24d1f
Vladimir Hejrosky <vladimir.hejrosky@gmail.com>
2025-03-27 18:03:57 +0100

    Add backdrop to  ToggleButton

--- app/tasks/components/ToggleTaskButton.tsx
+++ app/tasks/components/ToggleTaskButton.tsx
@@ -1,41 +1,55 @@
-"use client"
+"use client";
 import DoneIcon from "@mui/icons-material/Done";
 import ReplayIcon from "@mui/icons-material/Replay";
+import { Backdrop, CircularProgress } from "@mui/material";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import axios from "axios";
 
 interface Props {
-    id: number,
-    done: boolean
+  id: number;
+  done: boolean;
 }
 
-const ToggleTaskButton = ({id, done}: Props) => {
-    const queryClient = useQueryClient()
-    const mutation = useMutation({
-        mutationFn: async (updatedData: { done: boolean }) => {
-            return await axios.patch(`api/tasks/${id}`, updatedData)
-        },
-        onSuccess: () => {
-            queryClient.invalidateQueries({queryKey: ["tasks"]})
-        }
-    })
+const ToggleTaskButton = ({ id, done }: Props) => {
+  const queryClient = useQueryClient();
+  const mutation = useMutation({
+    mutationFn: async (updatedData: { done: boolean }) => {
+      return await axios.patch(`api/tasks/${id}`, updatedData);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["tasks"] });
+    },
+  });
 
-    const handleUpdate = () => {
-        const updatedData = {
-            done: !done
-        }
-        mutation.mutate(updatedData)
-    }
+  const handleUpdate = () => {
+    const updatedData = {
+      done: !done,
+    };
+    mutation.mutate(updatedData);
+  };
 
   return (
     <>
-    {done ? (
-        <ReplayIcon onClick={handleUpdate} color="secondary" fontSize="medium" sx={{cursor:"pointer"}}/>
-    ) : (
-        <DoneIcon onClick={handleUpdate} color="success" fontSize="medium"sx={{cursor:"pointer"}}/>
-    )}
+      {done ? (
+        <ReplayIcon
+          onClick={handleUpdate}
+          color="secondary"
+          fontSize="medium"
+          sx={{ cursor: "pointer" }}
+        />
+      ) : (
+        <DoneIcon
+          onClick={handleUpdate}
+          color="success"
+          fontSize="medium"
+          sx={{ cursor: "pointer" }}
+        />
+      )}
+      <Backdrop open={mutation.isPending}>
+        <CircularProgress color="inherit" />
+      </Backdrop>
     </>
-  )
-}
+  );
+};
 
-export default ToggleTaskButton
+export default ToggleTaskButton;
