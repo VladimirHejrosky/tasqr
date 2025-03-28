7a0e46b09f14bebc3737bf8f3e6a6149c183d45e
Vladimir Hejrosky <vladimir.hejrosky@gmail.com>
2025-03-28 10:40:29 +0100

    Add loading files to all pages

---
+++ app/tasks/new/loading.tsx
@@ -0,0 +1,15 @@
+import { Container, Skeleton, Stack } from '@mui/material'
+
+const LoadingNewTaskpage = () => {
+  return (
+    <Container maxWidth="sm">
+      <Stack spacing={2} my={2}>
+        <Skeleton variant="rectangular" width="100%" height={50} />
+        <Skeleton variant="rectangular" width="100%" height={50} />
+        <Skeleton variant="rectangular" width="100%" height={50} />
+      </Stack>
+    </Container>
+  )
+}
+
+export default LoadingNewTaskpage
